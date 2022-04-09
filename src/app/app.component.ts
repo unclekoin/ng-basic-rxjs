803dbfa56ba304d7e2024f40dddc985942dca89b
Pavel Koryakin <unclekoin@gmail.com>
2022-04-09 21:38:37 +0300

    init: start rsjx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'RxJS';
+  public title = 'RxJS';
+
+  constructor() {
+  }
+
 }
